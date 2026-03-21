4cb6a69ff2a3532b478d0375bb2c45539922464e
Sci_Fist <scifistofficial@gmail.com>
2026-03-21 15:22:53 +0100

    feat: Add VoxelWorldGeneration.cpp to centralize voxel world generation, including fixes for config value copies and initial chunk LOD.

--- FirstVoxel/Voxel/Core/World/VoxelWorldGeneration.cpp
+++ FirstVoxel/Voxel/Core/World/VoxelWorldGeneration.cpp
@@ -405,14 +405,14 @@ void AVoxelWorld::ProcessInitialPlayerSpawn()
     
     auto AddToSet = [&](const FIntVector& C) { SpawnSet.Add(C); };
 
-    // 1. Surrounding concentric 5x5x5 area of player spawn
-    for (int32 x=-2; x<=2; x++) for (int32 y2=-2; y2<=2; y2++) for (int32 z2=-2; z2<=2; z2++)
+    // 1. Surrounding concentric 13x13 area of player spawn (Cinematic Crater Bounds)
+    for (int32 x=-6; x<=6; x++) for (int32 y2=-6; y2<=6; y2++) for (int32 z2=-2; z2<=2; z2++)
         AddToSet(FIntVector(SpawnCoord.X + x, SpawnCoord.Y + y2, SpawnCoord.Z + z2));
 
     // 2. Add ground layer strictly beneath player if they spawn in the sky
     if (FMath::Abs(SpawnCoord.Z - GroundCoord.Z) > 1)
     {
-        for (int32 x=-2; x<=2; x++) for (int32 y2=-2; y2<=2; y2++)
+        for (int32 x=-6; x<=6; x++) for (int32 y2=-6; y2<=6; y2++)
         {
             AddToSet(FIntVector(SpawnCoord.X + x, SpawnCoord.Y + y2, GroundCoord.Z));
             AddToSet(FIntVector(SpawnCoord.X + x, SpawnCoord.Y + y2, GroundCoord.Z + 1));
